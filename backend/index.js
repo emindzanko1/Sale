18056a6ddbba52a22f48e29731fbfa4513fd93dc
emindzanko1 <edzanko1@etf.unsa.ba>
2023-12-20 12:16:41 +0100

    Implemented user's registration

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import pg from 'pg';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import bcrypt from 'bcrypt';
+const saltRounds = 10;
 
 dotenv.config();
 
@@ -34,6 +36,33 @@ db.connect();
 app.get('/', (req, res) => {
   console.log(req.token);
   res.send('Hello');
+});
+
+app.post('/register', (req, res) => {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    if (err) {
+      console.error('Error hashing password:', err);
+      return res.status(500).send('Error hashing password');
+    }
+
+    const email  = req.body.email;
+    const hashedPassword = hash;
+
+    const query = `
+      INSERT INTO users (email, password)
+      VALUES ($1, $2)
+    `;
+    const values = [email, hashedPassword];
+
+    db.query(query, values, (error, result) => {
+      if (error) {
+        console.error('Error inserting user:', error);
+        return res.status(500).send('Error inserting user');
+      }
+
+      res.status(201).send('User registered successfully');
+    });
+  });
 });
 
 app.listen(PORT, () => {
--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -6,20 +6,14 @@ export default function AuthenticationPage() {
 }
 
 export async function action({ request }) {
-  const searchParam = new URL(request.url).searchParams;
-  const mode = searchParam.get('mode') || 'login';
-
-  if (mode !== 'login' && mode !== 'register') {
-    throw json({ message: 'Invalid mode.' }, { status: 422 });
-  }
-
+  
   const data = await request.formData();
   const authData = {
     email: data.get('email'),
     password: data.get('password'),
   };
 
-  const response = await fetch('http://localhost:3000/' + mode, {
+  const response = await fetch('http://localhost:3000/register', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
